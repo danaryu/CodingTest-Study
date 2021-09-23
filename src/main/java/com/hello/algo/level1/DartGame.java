95b464797a52aa724c4fec2ae7572ef5542d0786
danaRyu <dana40426@gmail.com>
2021-09-23 12:17:38 +0900

    ~9/23

--- src/main/java/com/hello/algo/level1/DartGame.java
+++ src/main/java/com/hello/algo/level1/DartGame.java
@@ -21,33 +21,35 @@ public class DartGame {
         int answer = 0;
         String last = "";
 
-        int score = 0, score2 = 0;
+        int score = 0,preScore = 0;
         char[] charArray = dartResult.toCharArray();
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0 ; i<charArray.length ; i++) {
-            char c = charArray[i];
+            char current = charArray[i];
 
-            if (Character.isLetter(c)) {
-                /* c가 보너스일 때 */
-                if (c == 'S') score = (int) Math.pow(Integer.parseInt(last), 1);
-                if (c == 'D') score = (int) Math.pow(Integer.parseInt(last), 2);
-                if (c == 'T') score = (int) Math.pow(Integer.parseInt(last), 3);
+            /* c가 보너스일 때 */
+            if (Character.isLetter(current)) {
+                if (current == 'S') score = (int) Math.pow(Integer.parseInt(last), 1);
+                if (current == 'D') score = (int) Math.pow(Integer.parseInt(last), 2);
+                if (current == 'T') score = (int) Math.pow(Integer.parseInt(last), 3);
 
+                // 점수 초기화
                 last = "";
                 stack.push(score);
-            } else if (c == '*' || c == '#'){
-                /* c가 옵션일 때 (옵션 로직) */
 
-                if (c == '*') { /* 스타상 */
+            /* c가 옵션일 때 (옵션 로직) */
+            } else if (current == '*' || current == '#'){
+                if (current == '*') { /* 스타상 */
                     if (stack.size() >= 2) {
                         // 마지막 점수
                         score = stack.pop() * 2;
                         // 그 전 점수
-                        score2 = stack.pop() * 2;
-                        stack.push(score2);
+                        preScore = stack.pop() * 2;
+
+                        stack.push(preScore);
                         stack.push(score);
-                    } else { //하나 뿐일 때..
+                    } else { //하나 뿐일 때
                         score = stack.pop() * 2;
                         stack.push(score);
                     }
@@ -55,10 +57,11 @@ public class DartGame {
                     score = stack.pop() * -1;
                     stack.push(score);
                 }
+
+            /* c가 점수일 때, 두자리 정수일 때 대응 */
             } else {
-                /* c가 점수일 때 */
                 score = 0;
-                last += String.valueOf(c);
+                last += String.valueOf(current);
             }
         }
 
---
+++ src/main/java/com/hello/algo/level1/SortingString.java
@@ -0,0 +1,33 @@
+package com.hello.algo.level1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortingString {
+
+    public static void main(String[] args) {
+        String[] strings = {"abce", "abcd", "cdx"};
+        int n = 1;
+        System.out.println(solution(strings, n));
+    }
+
+    public static String[] solution(String[] strings, int n) {
+        // String[] answer = {};
+
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.charAt(n) < o2.charAt(n)) return -1;
+                else if (o1.charAt(n) > o2.charAt(n)) return 1;
+                else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+
+        for (String str : strings) System.out.println("str = " + str);
+
+        return strings;
+    }
+
+}
